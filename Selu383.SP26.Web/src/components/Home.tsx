9de2454bb8c72edb29ca8162f596461204d021b3
Eliora <181532355+ElioraBrowning@users.noreply.github.com>
2026-03-11 04:05:01 -0500

    LandingPage + SigninModal
    Getting ready for the presentation

--- Selu383.SP26.Web/src/components/Home.tsx
+++ Selu383.SP26.Web/src/components/Home.tsx
@@ -1,8 +1,19 @@
 function Home() {
   return (
-    <div>
-      <h1>Caffeinated Lions</h1>
-      <p>Pouring pride into every cup.</p>
+    <div className="card" style={{ padding: "2.5rem", margin: "2rem -3rem", backgroundColor: "rgba(255, 255, 255, 0.75)" }}>
+      <img
+        src="/src/assets/ConceptLogo2.png"
+        alt="Caffeinated Lions Logo"
+        style={{ display: "block", margin: "0 auto -3rem", width: "20rem" }}
+      />
+      <h1
+        style={{ textAlign: "center", width: "100%", fontSize: "300%" }}
+      >
+        Caffeinated Lions
+      </h1>
+      <p style={{ textAlign: "center", width: "100%", fontSize: "120%", marginTop: "-1.5rem" }}>
+        Pouring pride into every cup.
+      </p>
     </div>
   );
 }
