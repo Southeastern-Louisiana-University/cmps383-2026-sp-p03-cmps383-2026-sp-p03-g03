d60b3986f8b47d4c61c672514b9da27a3b265403
Cickks <michael.painter@selu.edu>
2026-04-22 00:18:24 -0500

    updated migrations for menu
    add migration for menu item location availability overrides

---
+++ Selu383.SP26.Api/Migrations/20260419101942_AddMenuItemLocationOverrides.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Selu383.SP26.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMenuItemLocationOverrides : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "menu_item_location_overrides",
+                columns: table => new
+                {
+                    MenuItemId = table.Column<int>(type: "int", nullable: false),
+                    LocationId = table.Column<int>(type: "int", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false),
+                    UnavailableReason = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    DisabledAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DisabledByUserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_menu_item_location_overrides", x => new { x.MenuItemId, x.LocationId });
+                    table.ForeignKey(
+                        name: "FK_menu_item_location_overrides_AspNetUsers_DisabledByUserId",
+                        column: x => x.DisabledByUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_menu_item_location_overrides_locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_menu_item_location_overrides_menu_items_MenuItemId",
+                        column: x => x.MenuItemId,
+                        principalTable: "menu_items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_menu_item_location_overrides_DisabledByUserId",
+                table: "menu_item_location_overrides",
+                column: "DisabledByUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_menu_item_location_overrides_LocationId",
+                table: "menu_item_location_overrides",
+                column: "LocationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "menu_item_location_overrides");
+        }
+    }
+}
