f64896dfc75c626fabb6469a5c03d61c8353721c
Cickks <michael.painter@selu.edu>
2026-04-25 04:06:57 -0500

    updated dtos

--- Selu383.SP26.Api/Features/Auth/UserDto.cs
+++ Selu383.SP26.Api/Features/Auth/UserDto.cs
@@ -12,7 +12,10 @@ public class UserDto
     public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
     public int LocationId { get; set; }
+    public string? LocationName { get; set; }
 
     public string[] Roles { get; set; } = Array.Empty<string>();
     public int LoyaltyPoints { get; set; }
+
+    public bool IsDisabled { get; set; }
 }
